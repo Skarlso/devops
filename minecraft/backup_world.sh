51896d7351f59a863f1041ca0cdfaef44a737d5b
Gergely Brautigam <skarlso777@gmail.com>
2016-04-16 20:57:57 +0200

    Added check for env variables that they are set.

--- minecraft/backup_world.sh
+++ minecraft/backup_world.sh
@@ -9,34 +9,38 @@ if [[ -t 1 ]]; then
     fi
 fi
 
+if [[ -z ${MINECRAFT_BUCKET} ]]; then
+	printf "Please set the env variable ${RED}MINECRAFT_BUCKET${NC} to the s3 archive bucket name.\n"
+	exit 0
+fi
+
+if [[ -z ${MINECRAFT_ARCHIVE_LIMIT} ]]; then
+	printf "Please set the env variable ${RED}MINECRAFT_ARCHIVE_LIMIT${NC} to limit the number of archives to keep.\n"
+	exit 0
+fi
+
 backup_bucket=${MINECRAFT_BUCKET}
 backup_limit=${MINECRAFT_ARCHIVE_LIMIT}
 world=$1
-printf "Creating archive of ${RED}${world}${NC}"
-echo
+printf "Creating archive of ${RED}${world}${NC}\n"
 archive_name="${world}-$(date +"%H-%M-%S-%m-%d-%Y").zip"
 zip -r $archive_name $world
 
-printf "Checking if bucket has more than ${RED}${backup_limit}${NC} files already."
-echo
+printf "Checking if bucket has more than ${RED}${backup_limit}${NC} files already.\n"
 content=( $(aws s3 ls s3://$backup_bucket | awk '{print $4}') )
 
 if [[ ${#content[@]} -eq $backup_limit || ${#content[@]} -gt $backup_limit  ]]; then
     echo "There are too many archives. Deleting oldest one."
     # We can assume here that the list is in cronological order
-	printf "${RED}s3://${backup_bucket}/${content[0]}"
-	echo
+	printf "${RED}s3://${backup_bucket}/${content[0]}\n"
     aws s3 rm s3://$backup_bucket/${content[0]}
 fi
 
-printf "Uploading ${RED}${archive_name}${NC} to s3 archive bucket."
-echo
+printf "Uploading ${RED}${archive_name}${NC} to s3 archive bucket.\n"
 state=$(aws s3 cp $archive_name s3://$backup_bucket)
 
 if [[ "$state" =~ "upload:" ]]; then
-    printf "File upload ${LIGHT_GREEN}successful${NC}."
-	echo
+    printf "File upload ${LIGHT_GREEN}successful${NC}.\n"
 else
-    printf "${RED}Error${NC} occured while uploading archive. Please investigate."
-	echo
+    printf "${RED}Error${NC} occured while uploading archive. Please investigate.\n"
 fi
