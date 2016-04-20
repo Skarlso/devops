2be4628d8548cf123fefd133d0ce544986275e10
Gergely Brautigam <skarlso777@gmail.com>
2016-04-20 21:59:53 +0200

    Added check to see if the container really stopped.

--- minecraft/stop_server.sh
+++ minecraft/stop_server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-eval $(docker-machine env)
+eval "$(docker-machine env)"
 state=$(docker stop mc_server)
 if [[ $state =~ "No such container" ]]; then
     echo "Failed to stop container!"
     exit 1
+else
+    printf "%s\n" "Docker container stopped."
 fi
 
 docker-machine stop
